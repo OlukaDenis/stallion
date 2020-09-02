4d47e40e2e0c398e54aa9e47a6d1d1d12b0915ca
Kelvin Chot <kinyunye@gmail.com>
2020-09-02 18:47:18 +0300

    Validate user input before proceeding to next stage of quote generation

--- src/components/QuotationGenerator.jsx
+++ src/components/QuotationGenerator.jsx
@@ -1,21 +1,14 @@
 import { DatePicker, Tooltip, Button, Divider, Alert, Input } from 'antd';
-import {
-  FlagFilled,
-  FlagOutlined,
-  RightOutlined,
-  UserOutlined,
-  MailOutlined,
-  PhoneOutlined,
-} from '@ant-design/icons';
+import { FlagFilled, FlagOutlined, RightOutlined, UserOutlined, MailOutlined, PhoneOutlined } from '@ant-design/icons';
 import { connect } from 'react-redux';
 import { withTranslation } from '../utilities/i18n';
 import LocationSelector from './LocationSelector';
-import { useState } from 'react';
 import { bindActionCreators } from 'redux';
 import { setOrigin, setDestination, setCars, setPickupDate, setEmail, setName, setPhone } from '../state/quote/action';
 import moment from 'moment';
 import ClearableInputElement from './ClearableInputElement';
 import SelectedCars from './SelectedCars';
+import { useState } from 'react';
 
 export function QuotationGenerator({
   theme,
@@ -27,7 +20,87 @@ export function QuotationGenerator({
   setEmail,
   setPhone,
 }) {
+
+
+  const [hasOriginError, setHasOriginError] = useState(false);
+  const [hasDestinationError, setHasDestinationError] = useState(false);
+
+  const [hasSelectedCarsError, setHasSelectedCarsError] = useState(false);
+  
+  const [hasPickupDateError, setHasPickupDateError] = useState(false);
+  const [hasNameError, setHasNameError] = useState(false);
+  const [hasPhoneError, setHasPhoneError] = useState(false);
+  const [hasEmailError, setHasEmailError] = useState(false);
+
   const isLightMode = theme === 'light';
+  const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const validateInput = () => {
+    let isErrorFound = false;
+
+    if ((quote.origin.match(/,/g) || []).length) {
+      setHasOriginError(false);
+    } else {
+      isErrorFound = true;
+      setHasOriginError(true);
+    }
+
+    if ((quote.destination.match(/,/g) || []).length) {
+      setHasDestinationError(false);
+    } else {
+      isErrorFound = true;
+      setHasDestinationError(true);
+    }
+
+    if (Object.keys(quote.cars).length > 0) {
+      setHasSelectedCarsError(false);
+    } else {
+      isErrorFound = true;
+      setHasSelectedCarsError(true);
+    }
+
+    if (quote.pickupDate) {
+      setHasPickupDateError(false);
+    } else {
+      isErrorFound = true;
+      setHasPickupDateError(true);
+    }
+
+    if (quote.name) {
+      setHasNameError(false);
+    } else {
+      isErrorFound = true;
+      setHasNameError(true);
+    }
+
+    if (isValidEmail(quote.email)) {
+      setHasEmailError(false);
+    } else {
+      isErrorFound = true;
+      setHasEmailError(true);
+    }
+
+    if (isValidPhoneNumber(quote.phone)) {
+      setPhone(isValidPhoneNumber(quote.phone));
+      setHasPhoneError(false);
+    } else {
+      isErrorFound = true;
+      setHasPhoneError(true);
+    }
+
+    return isErrorFound;
+  };
+
+
+  const calculateQuote = () => {
+    console.log('Validating quote={}');
+    if (validateInput()) return;
+    console.log(quote);
+  };
+
+  const isValidPhoneNumber = (phone) => (phoneRegex.test(phone) ? phone.replace(phoneRegex, '($1) $2-$3') : null);
+  const isValidEmail = (email) => (emailRegex.test(email) ? email : null);
 
   return (
     <>
@@ -64,23 +137,29 @@ export function QuotationGenerator({
               title="Begin typing a zip code or city and then select a suggested location"
             >
               <LocationSelector
+                value={quote.origin}
                 onSelect={(value) => setOrigin(value)}
                 placeholder="Pickup Location"
                 icon={<FlagFilled />}
               />
+              {hasOriginError ? <Alert message="Kindly select a valid origin" type="error" /> : <></>}
             </Tooltip>
 
-            
-            
             <Tooltip
               trigger={['click', 'hover']}
               title="Begin typing a zip code or city and then select a suggested location"
             >
               <LocationSelector
+                value={quote.destination}
                 onSelect={(value) => setDestination(value)}
                 placeholder="Delivery Location"
                 icon={<FlagOutlined />}
               />
+              {hasDestinationError ? (
+                <Alert message="Kindly select a valid delivery destination" type="error" />
+              ) : (
+                <></>
+              )}
             </Tooltip>
           </div>
 
@@ -106,6 +185,11 @@ export function QuotationGenerator({
             }
           >
             <SelectedCars />
+            {hasSelectedCarsError ? (
+              <Alert message="Kindly add a car, select in order Year > Make > Model" type="error" />
+            ) : (
+              <></>
+            )}
           </div>
 
           <div className={isLightMode ? 'quotation_section' : 'quotation_section quotation_section_dark'}>
@@ -144,34 +228,38 @@ export function QuotationGenerator({
                   setPickupDate(date == null ? '' : date.format('YYYY-MM-DD'));
                 }}
               />
+              {hasPickupDateError ? <Alert message="Kindly select a date for vehicle pickup" type="error" /> : <></>}
             </Tooltip>
 
             {quote.pickupDate ? (
               <div>
-                
                 <ClearableInputElement value={quote.name} onChange={setName} placeholder="Name" Icon={UserOutlined} />
-                
-                
+                {hasNameError ? <Alert message="Kindly enter the shipper's name" type="error" /> : <></>}
+
                 <ClearableInputElement
+                  type="email"
                   value={quote.email}
                   onChange={setEmail}
                   placeholder="Email"
                   Icon={MailOutlined}
                 />
-                
-                
+                {hasEmailError ? <Alert message="Kindly enter a valid email address" type="error" /> : <></>}
+
                 <ClearableInputElement
+                  type="tel"
                   value={quote.phone}
                   onChange={setPhone}
                   placeholder="Phone"
                   Icon={PhoneOutlined}
                 />
+
+                {hasPhoneError ? <Alert message="Kindly enter a valid phone number" type="error" /> : <></>}
               </div>
             ) : (
               <></>
             )}
           </div>
-          <Button type="primary" shape="round" size="large" block>
+          <Button onClick={calculateQuote} type="primary" shape="round" size="large" block>
             Calculate Quote <RightOutlined />
           </Button>
         </div>
