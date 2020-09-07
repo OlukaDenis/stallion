234788034e2b29ff63f3f49bacaa29201930e10c
Kelvin Chot <kinyunye@gmail.com>
2020-09-07 13:58:53 +0300

    responsive quotation summary display UI

---
+++ src/components/QuotationSummary.jsx
@@ -0,0 +1,80 @@
+export function QuotationSummary({ quote, theme }) {
+  return (
+    <div className="quotation-summary">
+      <div className="header-items">
+        <div className="item-cell cell-1">Quote #</div>
+        <div className="item-cell cell-2">Pickup From</div>
+        <div className="item-cell cell-3">Deliver To</div>
+        <div className="item-cell cell-4">Shipping Rate</div>
+      </div>
+      <div className="service-items">
+        <div className="item-cell cell-1">909090</div>
+        <div className="item-cell cell-2">{quote.origin}</div>
+        <div className="item-cell cell-3">{quote.destination}</div>
+        <div className="item-cell cell-4">$1,965</div>
+      </div>
+      <style jsx>
+        {`
+          .quotation-summary {
+            padding-bottom: 10px;
+          }
+          @media only screen and (min-width: 720px) {
+            .quotation-summary .header-items,
+            .quotation-summary .service-items {
+              display: flex;
+              min-height: 35px;
+            }
+            .quotation-summary .header-items {
+              background: rgba(248, 105, 66, 0.3);
+            }
+            .quotation-summary .item-cell {
+              border: 0.5px dotted grey;
+              flex: 3;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              overflow-wrap: anywhere;
+            }
+            .quotation-summary .cell-1,
+            .quotation-summary .cell-4 {
+              flex: 1.5;
+            }
+          }
+          @media only screen and (max-width: 330px) {
+            .quotation-summary {
+              font-size: 12px;
+            }
+          }
+          @media only screen and (max-width: 720px) {
+            .quotation-summary {
+              white-space: nowrap;
+              display: flex;
+            }
+            .quotation-summary .header-items {
+              display: inline-block;
+              flex: 2;
+            }
+            .quotation-summary .service-items {
+              display: inline-block;
+              flex: 3;
+            }
+            .quotation-summary .header-items,
+            .quotation-summary .service-items {
+              min-height: 35px;
+            }
+            .quotation-summary .header-items {
+              background: rgba(248, 105, 66, 0.3);
+            }
+            .quotation-summary .service-items .item-cell {
+              padding-left: 6px;
+            }
+            .quotation-summary .item-cell {
+              border: 0.5px dotted grey;
+              padding-right: 3px;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+}
