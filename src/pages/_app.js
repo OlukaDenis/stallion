b5dae65ea621aa7709b5712db4e4b894e9810093
Kelvin Chot <kinyunye@gmail.com>
2020-09-22 10:54:25 +0300

    Make the delivery steps responsive, hide map in small devices...

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -300,8 +300,10 @@ const MyApp = ({ Component, pageProps }) => {
                 line-height: 1.6;
               }
             }
-
             @media only screen and (max-width: 400px) {
+              .ant-steps-label-vertical .ant-steps-item-content {
+                width: 92px;
+              }
               .theme-mode-switch {
                 display: none;
               }
@@ -311,6 +313,11 @@ const MyApp = ({ Component, pageProps }) => {
                 text-align: center;
                 margin-top: 5px;
                 margin-bottom: 5px;
+              }
+            }
+            @media only screen and (max-width: 300px) {
+              .ant-steps-label-vertical .ant-steps-item-content {
+                width: 78px;
               }
             }
             /* INDEX-PAGE::START */
@@ -338,91 +345,91 @@ const MyApp = ({ Component, pageProps }) => {
 
             /* QUOTATION-COMPONENTS::START */
             .quotation {
-            z-index: 3;
-            right: 0;
-            padding-bottom: 3px;
-            border: 2px solid #fff;
-            box-shadow: 0 0 0 0.5rem rgba(0, 0, 0, 0.4);
-            background: #fff;
-          }
+              z-index: 3;
+              right: 0;
+              padding-bottom: 3px;
+              border: 2px solid #fff;
+              box-shadow: 0 0 0 0.5rem rgba(0, 0, 0, 0.4);
+              background: #fff;
+            }
 
-          .quotation_header {
-            text-align: center;
-            width: 100%;
-            padding-top: 1.2rem;
-            padding-bottom: 1.2rem;
-            background-color: #f63e0c;
-            background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEoAAABMAgMAAAC358n/AAAACVBMVEX///9HcEz///9EorLRAAAAA3RSTlMaAA4Mf89/AAABQElEQVQ4y3WUMVLEMAxFHWZSQEWzR9h77BFSxJkt9yhbAhUNPQ0FnBL/SNr5X3bcSHnOWN+SrPKHVesG81uxlnIvpTw1LyzYrfnlS9kCdqo19sCu8M/wbA/eBv+51thjtroFi8M3Zhd8sJjGImDsgcXhDzHEHoEb2w+flFX4LxQYLAIyy1lg9u0WLIsBW1MWwCQzs7GcBWZhd3bzFDNLgXt2MpZKIsztzpIYY1oSY1oSYypGmVljWhJj1ZkFdsZiJmdSksR2MZ/GRAyz2Zv1tXhbt/Wj/0mmVcuIsWbp89nY6mH5vqNcxTVGjHMvaXEm6Tuu+VWkHPeQhHV20cdw1Luprfq+Px+/mWXARm8wTyxi/Ka1GMa0GMezRIuhjGcTj43JWR4vMf8mnYnduCLGM7Ybf411Y7Kxt7Y+WljYd2P/340aPNbGbJEAAAAASUVORK5CYII=);
-            background-repeat: no-repeat;
-            background-size: auto 100%;
-            color: #fff;
-          }
+            .quotation_header {
+              text-align: center;
+              width: 100%;
+              padding-top: 1.2rem;
+              padding-bottom: 1.2rem;
+              background-color: #f63e0c;
+              background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEoAAABMAgMAAAC358n/AAAACVBMVEX///9HcEz///9EorLRAAAAA3RSTlMaAA4Mf89/AAABQElEQVQ4y3WUMVLEMAxFHWZSQEWzR9h77BFSxJkt9yhbAhUNPQ0FnBL/SNr5X3bcSHnOWN+SrPKHVesG81uxlnIvpTw1LyzYrfnlS9kCdqo19sCu8M/wbA/eBv+51thjtroFi8M3Zhd8sJjGImDsgcXhDzHEHoEb2w+flFX4LxQYLAIyy1lg9u0WLIsBW1MWwCQzs7GcBWZhd3bzFDNLgXt2MpZKIsztzpIYY1oSY1oSYypGmVljWhJj1ZkFdsZiJmdSksR2MZ/GRAyz2Zv1tXhbt/Wj/0mmVcuIsWbp89nY6mH5vqNcxTVGjHMvaXEm6Tuu+VWkHPeQhHV20cdw1Luprfq+Px+/mWXARm8wTyxi/Ka1GMa0GMezRIuhjGcTj43JWR4vMf8mnYnduCLGM7Ybf411Y7Kxt7Y+WljYd2P/340aPNbGbJEAAAAASUVORK5CYII=);
+              background-repeat: no-repeat;
+              background-size: auto 100%;
+              color: #fff;
+            }
 
-          .quotation_header_dark {
-            color: rgba(255, 255, 255, 0.9);
-            background-color: #000;
-          }
+            .quotation_header_dark {
+              color: rgba(255, 255, 255, 0.9);
+              background-color: #000;
+            }
 
-          .quotation_header h3 {
-            font-weight: 700;
-            font-size: 18px;
-            border-bottom: 1px solid black;
-            padding-bottom: 0.8rem;
-            margin: 0 1.5rem 0.5rem;
-            color: #fff;
-          }
-          .quotation_header_dark h3 {
-            color: rgba(255, 255, 255, 0.9);
-          }
+            .quotation_header h3 {
+              font-weight: 700;
+              font-size: 18px;
+              border-bottom: 1px solid black;
+              padding-bottom: 0.8rem;
+              margin: 0 1.5rem 0.5rem;
+              color: #fff;
+            }
+            .quotation_header_dark h3 {
+              color: rgba(255, 255, 255, 0.9);
+            }
 
-          .quotation_header p {
-            font-size: 14px;
-            font-weight: 400;
-            margin-bottom: 0;
-            line-height: 1.8rem;
-            opacity: 1;
-          }
+            .quotation_header p {
+              font-size: 14px;
+              font-weight: 400;
+              margin-bottom: 0;
+              line-height: 1.8rem;
+              opacity: 1;
+            }
 
-          .quotation_body {
-            text-align: center;
-            padding: 0.6rem 0.6rem 0.2rem;
-          }
+            .quotation_body {
+              text-align: center;
+              padding: 0.6rem 0.6rem 0.2rem;
+            }
 
-          .quotation_section {
-            background: #000;
-            padding: 0.2rem 1.4rem;
-            display: flex;
-            color: #fff;
-            box-sizing: border-box;
-          }
-          .quotation_section_dark {
-            color: rgba(255, 255, 255, 0.9);
-          }
-          .quotation_section--step {
-            font-size: 17px;
-            text-align: center;
-            font-weight: 700;
-            padding-right: 1.1rem;
-            border-right: 1px solid hsla(0, 0%, 100%, 0.3);
-            margin: 0.6rem 0;
-          }
+            .quotation_section {
+              background: #000;
+              padding: 0.2rem 1.4rem;
+              display: flex;
+              color: #fff;
+              box-sizing: border-box;
+            }
+            .quotation_section_dark {
+              color: rgba(255, 255, 255, 0.9);
+            }
+            .quotation_section--step {
+              font-size: 17px;
+              text-align: center;
+              font-weight: 700;
+              padding-right: 1.1rem;
+              border-right: 1px solid hsla(0, 0%, 100%, 0.3);
+              margin: 0.6rem 0;
+            }
 
-          .quotation_section--title {
-            font-size: 16px;
-            padding-left: 1.1rem;
-            line-height: 2.05rem;
-            margin: 0.6rem 0;
-          }
+            .quotation_section--title {
+              font-size: 16px;
+              padding-left: 1.1rem;
+              line-height: 2.05rem;
+              margin: 0.6rem 0;
+            }
 
-          .quotation_input-container {
-            border: 1px solid #ccc;
-            border-top: 0;
-            padding: 1rem;
-            margin-bottom: 1rem;
-          }
-          .quotation_input-container_dark {
-            background: #f0f0f0;
-          }
+            .quotation_input-container {
+              border: 1px solid #ccc;
+              border-top: 0;
+              padding: 1rem;
+              margin-bottom: 1rem;
+            }
+            .quotation_input-container_dark {
+              background: #f0f0f0;
+            }
             /* QUOTATION-COMPONENTS::END */
           `}
         </style>
